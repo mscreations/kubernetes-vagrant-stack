0933571475551291f6b3485e65d84f9bed962918
Jon Shaulis <jon@mscreations.net>
2025-09-27 22:38:54 -0400

    Reenable reset script

--- reset_uuid.ps1
+++ reset_uuid.ps1
@@ -5,21 +5,21 @@ param (
 
 Write-Host "Resetting UUID for $VMName"
 
-# $MSVM = gwmi -Namespace root\virtualization\v2 -Class msvm_computersystem -Filter "ElementName = '$VMName'"
+$MSVM = gwmi -Namespace root\virtualization\v2 -Class msvm_computersystem -Filter "ElementName = '$VMName'"
  
-# # get current settings object
-# $MSVMSystemSettings = $null
-# foreach($SettingsObject in $MSVM.GetRelated('msvm_virtualsystemsettingdata'))
-# {
-#     $MSVMSystemSettings = [System.Management.ManagementObject]$SettingsObject
-# }
+# get current settings object
+$MSVMSystemSettings = $null
+foreach($SettingsObject in $MSVM.GetRelated('msvm_virtualsystemsettingdata'))
+{
+    $MSVMSystemSettings = [System.Management.ManagementObject]$SettingsObject
+}
  
-# # assign a new id
-# $MSVMSystemSettings['BIOSGUID'] = "{$(([System.Guid]::NewGuid()).Guid.ToUpper())}"
+# assign a new id
+$MSVMSystemSettings['BIOSGUID'] = "{$(([System.Guid]::NewGuid()).Guid.ToUpper())}"
  
-# $VMMS = gwmi -Namespace root\virtualization\v2 -Class msvm_virtualsystemmanagementservice
-# # prepare and assign parameters
-# $ModifySystemSettingsParameters = $VMMS.GetMethodParameters('ModifySystemSettings')
-# $ModifySystemSettingsParameters['SystemSettings'] = $MSVMSystemSettings.GetText([System.Management.TextFormat]::CimDtd20)
-# # invoke modification
-# $VMMS.InvokeMethod('ModifySystemSettings', $ModifySystemSettingsParameters, $null)
+$VMMS = gwmi -Namespace root\virtualization\v2 -Class msvm_virtualsystemmanagementservice
+# prepare and assign parameters
+$ModifySystemSettingsParameters = $VMMS.GetMethodParameters('ModifySystemSettings')
+$ModifySystemSettingsParameters['SystemSettings'] = $MSVMSystemSettings.GetText([System.Management.TextFormat]::CimDtd20)
+# invoke modification
+$VMMS.InvokeMethod('ModifySystemSettings', $ModifySystemSettingsParameters, $null)
