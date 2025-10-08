6857f7175f34897cbfd1979137f51defa32faf9e
Jon Shaulis <jon@mscreations.net>
2025-10-08 18:25:25 -0400

    Refactor playbook iteration to use sorted list of YAML files

--- scripts/execute_ansible_folder.sh
+++ scripts/execute_ansible_folder.sh
@@ -19,7 +19,7 @@ fi
 
 shopt -s nullglob
 
-for playbook in "$folder"/*.y{a,}ml; do
+for playbook in $(ls "$folder"/*.y{a,}ml | sort -V); do
   echo "Running playbook: $playbook"
   ansible-playbook -i inventory.ini "$playbook"
 done
